76e5c5b198958fff316b0890ea20f8ff8ee14adc
Hynkzz <fafeaf42@gmail.com>
2025-10-18 14:07:42 +0300

    Hesap makinesini bitirdim.

--- bmghm.cpp
+++ bmghm.cpp
@@ -13,11 +13,11 @@ int main(){
 	cout<<"Carpma icin 3 yaziniz"<<endl;
 	cout<<"Bolme icin 4 yaziniz"<<endl;
 	
-	cout<<"Hangi islemi yapmak istiyorsunuz:";
+	cout<<"Hangi islemi yapmak istiyorsunuz?";
 	cin>>islem;
 	
 	if (islem == "1"){
-		cout<<"Sayi giriniz (cikmak icin 0 girin): ";
+		cout<<"Sayi giriniz (cikmak icin 0 giriniz): ";
 		cin>>sayi;
 		toplam=sayi;
 		
@@ -32,9 +32,55 @@ int main(){
 						toplam+=sayi;
 		}
 	} while (sayi!=0);
-	cout<<"sayilarin toplamlari"<<toplam<<endl;}
-
-//
+	cout<<"sayilarin toplamlari "<<toplam<<endl;}
+}
+	else if (islem == "2") { 
+		cout << "Sayi giriniz (cikmak icin 0 girin): "; 
+		cin >> sayi; 
+		cikarma = sayi;  
+			if (sayi != 0) { 
+				do { 
+					cout << "Sayi giriniz (cikmak icin 0 girin): "; 
+					cin >> sayi; 
+						if (sayi != 0) { 
+						cikarma -= sayi; 
+	 }  
+	 } while (sayi != 0); 
+	 	cout << "Sayilarin farklari: " << cikarma << endl; }
+		 }
+		else if (islem == "3") { 
+		cout << "Sayi giriniz (cikmak icin 0 girin): "; 
+		cin >> sayi; 
+		carpma = sayi;  
+			if (sayi != 0) { 
+				do { 
+					cout << "Sayi giriniz (cikmak icin 0 girin): "; 
+					cin >> sayi; 
+						if (sayi != 0) { 
+						carpma *= sayi; 
+	 }  
+	 } while (sayi != 0); 
+	 	cout << "Sayilarin carpimlari: " << carpma << endl; }
+		 }
+				else if (islem == "4") { 
+		cout << "Sayi giriniz (cikmak icin 0 girin): "; 
+		cin >> sayi; 
+		bolme = sayi;  
+			if (sayi != 0) { 
+				do { 
+					cout << "Sayi giriniz (cikmak icin 0 girin): "; 
+					cin >> sayi; 
+						if (sayi != 0) { 
+						bolme/= sayi; 
+	 }  
+	 } while (sayi != 0); 
+	 	cout << "Sayilarin bolumleri: " << bolme << endl; }
+		 }
+			
+	else 
+	{
+		cout<<"gecersiz islem girdiniz ";
+	}
 		return 0;
 }
 
