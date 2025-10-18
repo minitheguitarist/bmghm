052a78b68ff42d07c1434356caf29d84dd14080b
SurhayKoc <surhaykoc1@gmail.com>
2025-10-18 10:59:30 +0300

    hesap makinesine ekleme yapıldı

--- bmghm.cpp
+++ bmghm.cpp
@@ -1,11 +1,41 @@
-#inculde <iostream>
+#include <iostream>
+using namespace std;
 
-using namespaces std;
+int main(){
+	
+	float sayi,toplam,cikarma,carpma,bolme;
+	string islem;
+	
+	cout<<"Hesap makinesi"<<endl;
+	
+	cout<<"Toplama icin 1 yaziniz"<<endl;
+	cout<<"Cikarma icin 2 yaziniz"<<endl;
+	cout<<"Carpma icin 3 yaziniz"<<endl;
+	cout<<"Bolme icin 4 yaziniz"<<endl;
+	
+	cout<<"Hangi islemi yapmak istiyorsunuz:";
+	cin>>islem;
+	
+	if (islem == "1"){
+		cout<<"Sayi giriniz (cikmak icin 0 girin): ";
+		cin>>sayi;
+		toplam=sayi;
+		
+		if (sayi!=0){
+		
+			do{
+				cout<<"sayi giriniz(cikmak icin 0 giriniz)";
+				cin>>sayi;
+					if(sayi!=0){
+		
+			
+						toplam+=sayi;
+		}
+	} while (sayi!=0);
+	cout<<"sayilarin toplamlari"<<toplam<<endl;}
 
-int main() {
-
-cout<<"Odevimiz Bilgisayar Muhendisligine Giris"<< endl;
-
-
-return 0;
+//
+		return 0;
 }
+
+
